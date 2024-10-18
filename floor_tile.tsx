160f6a7939ffc676ae2520b9583fab81eb757f69
Luka Micheletti <l.micheletti@ebw.it>
2024-10-19 00:21:21 +0200

    added lv1

---
+++ floor_tile.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="floor_tile" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="floor_tile.png" width="16" height="16"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="1,0,1,0,1,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
