7457a4a6591f59f08aab603dc33ef493661a97bc
Luka Micheletti <michelettiluka@gmail.com>
2024-11-23 02:47:52 +0100

    Restored a lot

--- src/main.py
+++ src/main.py
@@ -127,6 +127,15 @@ class FishGame:
         # Add children to the active scene.
         # uniques.ACTIVE_SCENE.add_child(fish)
 
+
+        ########################
+        # Physics timestep.
+        ########################
+        self.phys_accumulated_time: float = 0.0
+        self.phys_timestep: float = 1.0 / (0.5 * SETTINGS[Keys.TARGET_FPS])
+        ########################
+        ########################
+
     def __create_window(self) -> pyglet.window.BaseWindow:
         window = pyglet.window.Window(
             width = SETTINGS[Keys.WINDOW_WIDTH] if not SETTINGS[Keys.FULLSCREEN] else None,
@@ -175,14 +184,25 @@ class FishGame:
 
     def update(self, dt: float) -> None:
         # upscaler_program["dt"] = dt
-        # Benchmark measures update time.
-        # Compute collisions through collision manager.
-        controllers.COLLISION_CONTROLLER.update(dt = dt)
+
+        self.phys_accumulated_time += dt
+        # print("UPDATE", dt, self.phys_accumulated_time)
+
+        while self.phys_accumulated_time >= self.phys_timestep:
+            # print("FIXED_UPDATE", dt, self.phys_accumulated_time, self.phys_timestep)
+            with controllers.INPUT_CONTROLLER:
+                if uniques.ACTIVE_SCENE is not None:
+                    # uniques.ACTIVE_SCENE.fixed_update(self.phys_timestep)
+                    uniques.ACTIVE_SCENE.update(self.phys_timestep)
+                    self.phys_accumulated_time -= self.phys_timestep
+
+            # Compute collisions through collision manager.
+            controllers.COLLISION_CONTROLLER.update(dt = dt)
 
         # InputController makes sure every input is handled correctly.
-        with controllers.INPUT_CONTROLLER:
-            if uniques.ACTIVE_SCENE is not None:
-                uniques.ACTIVE_SCENE.update(dt = dt)
+        # with controllers.INPUT_CONTROLLER:
+        #     if uniques.ACTIVE_SCENE is not None:
+        #         uniques.ACTIVE_SCENE.update(dt = dt)
 
     def run(self) -> None:
         pyglet.clock.schedule_interval(self.update, 1.0 / (2.0 * SETTINGS[Keys.TARGET_FPS]))
