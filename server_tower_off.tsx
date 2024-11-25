3429a0db40bf3827e821df31a3fa7bca964982af
Luka Micheletti <michelettiluka@gmail.com>
2024-11-25 14:48:40 +0100

    2024_11_22 art

---
+++ server_tower_off.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="server_tower_off" tilewidth="16" tileheight="16" tilecount="4" columns="4">
+ <image source="server_tower_off.png" width="64" height="16"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="400"/>
+   <frame tileid="2" duration="400"/>
+  </animation>
+ </tile>
+</tileset>
