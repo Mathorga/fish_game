d4f505d37522896d943f47bdc9a0907f9254cca4
Simone Branchesi <simobranca1@gmail.com>
2025-03-19 22:52:06 +0100

    added animations and tiles

--- water.tsx
+++ water.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="water" tilewidth="16" tileheight="16" tilecount="8" columns="8">
- <image source="running_water.png" width="128" height="16"/>
+<tileset version="1.9" tiledversion="1.9.2" name="water tileset" tilewidth="16" tileheight="16" tilecount="24" columns="8">
+ <image source="water_tileset.png" width="128" height="48"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="100"/>
@@ -11,6 +11,30 @@
    <frame tileid="5" duration="100"/>
    <frame tileid="6" duration="100"/>
    <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <animation>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
   </animation>
  </tile>
 </tileset>
