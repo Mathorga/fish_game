f3dd750b3c7235b7affd700dcee25b8e6a397924
Simone Branchesi <simobranca1@gmail.com>
2025-07-19 00:54:57 +0200

    added door sprite

--- alt_background.tsx
+++ alt_background.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="alt. background" tilewidth="16" tileheight="16" tilecount="25" columns="5">
- <image source="alt_background_tileset.png" width="80" height="80"/>
+ <image source="alt_background.png" width="80" height="80"/>
 </tileset>
